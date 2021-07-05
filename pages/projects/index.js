696e4795d5e374c886da5c7ae6298e9f34b95a05
Frederico Ramos <frederico.jlr@gmail.com>
2021-07-05 11:30:25 +0100

    page - project list 01.2
    filters list structure and functionality

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -13,26 +13,26 @@ export default function Projects() {
               <p>Filters</p>
             </button>
           </div>
-          <div id="filters-container">
+          <div id="filters-container" class="toggle-on">
+            <div id="filters-wrapper">
+
             <div id="filter-status" className="filter-list">
               <h6>Status:</h6>
               <ul>
                 <li>
-                  <label for="tag-online">
+                  <label htmlFor="tag-online">
                     <div className="left-semi-circle">
-                      <input type="checkbox" id="tag-online" class="tag-input" name="online" />
-                      <div className="bullet-circle">
-                        <Image src="/icons/tag-bullet.svg" width={0} height={0} />
-                      </div>
+                      <input type="checkbox" id="tag-online" className="tag-input" name="online" />
+                      <div className="bullet-circle"></div>
                     </div>
                     <div className="tag-name">Online</div>
                     <div className="right-semi-circle"></div>
                   </label>
                 </li>
                 <li>
-                  <label for="tag-development">
+                  <label htmlFor="tag-development">
                     <div className="left-semi-circle">
-                      <input type="checkbox" id="tag-development" class="tag-input" name="development" />
+                      <input type="checkbox" id="tag-development" className="tag-input" name="development" />
                       <div className="bullet-circle"></div>
                     </div>
                     <div className="tag-name">Development</div>
@@ -40,12 +40,10 @@ export default function Projects() {
                   </label>
                 </li>
                 <li>
-                  <label for="tag-maintenance">
+                  <label htmlFor="tag-maintenance">
                     <div className="left-semi-circle">
-                      <input type="checkbox" id="tag-maintenance" class="tag-input" name="maintenance" />
-                      <div className="bullet-circle">
-                        <Image src="/icons/tag-bullet.svg" width={0} height={0} />
-                      </div>
+                      <input type="checkbox" id="tag-maintenance" className="tag-input" name="maintenance" />
+                      <div className="bullet-circle"></div>
                     </div>
                     <div className="tag-name">Maintenance</div>
                     <div className="right-semi-circle"></div>
@@ -56,11 +54,58 @@ export default function Projects() {
             <div id="filter-tags" className="filter-list">
               <h6>Tags:</h6>
               <ul>
-                <li>Logo Design</li>
-                <li>Web Design</li>
-                <li>Development</li>
-                <li>Other</li>
+                <li>
+                  <label htmlFor="tag-logo-design">
+                    <div className="left-semi-circle">
+                      <input type="checkbox" id="tag-logo-design" className="tag-input" name="logo-design" />
+                      <div className="bullet-circle"></div>
+                    </div>
+                    <div className="tag-name">Logo Design</div>
+                    <div className="right-semi-circle"></div>
+                  </label>
+                </li>
+                <li>
+                  <label htmlFor="tag-web-design">
+                    <div className="left-semi-circle">
+                      <input type="checkbox" id="tag-web-design" className="tag-input" name="web-design" />
+                      <div className="bullet-circle"></div>
+                    </div>
+                    <div className="tag-name">Web Design</div>
+                    <div className="right-semi-circle"></div>
+                  </label>
+                </li>
+                <li>
+                  <label htmlFor="tag-web-development">
+                    <div className="left-semi-circle">
+                      <input type="checkbox" id="tag-web-development" className="tag-input" name="web-development" />
+                      <div className="bullet-circle"></div>
+                    </div>
+                    <div className="tag-name">Web Development</div>
+                    <div className="right-semi-circle"></div>
+                  </label>
+                </li>
+                <li>
+                  <label htmlFor="tag-branding">
+                    <div className="left-semi-circle">
+                      <input type="checkbox" id="tag-branding" className="tag-input" name="branding" />
+                      <div className="bullet-circle"></div>
+                    </div>
+                    <div className="tag-name">Web Development</div>
+                    <div className="right-semi-circle"></div>
+                  </label>
+                </li>
+                <li>
+                  <label htmlFor="tag-other">
+                    <div className="left-semi-circle">
+                      <input type="checkbox" id="tag-other" className="tag-input" name="other" />
+                      <div className="bullet-circle"></div>
+                    </div>
+                    <div className="tag-name">Other</div>
+                    <div className="right-semi-circle"></div>
+                  </label>
+                </li>
               </ul>
+            </div>
             </div>
           </div>
         </div>
