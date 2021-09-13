25fec61963da0a0d806ee8a55c2dcdac6b60bdb2
Frederico Ramos <frederico.jlr@gmail.com>
2021-09-13 21:33:36 +0100

    vercel - try01

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,7 @@
 import Head from 'next/head'
 import Home from './Home'
-// import BgPattern from 'components/Background'
+
 import { request } from "../lib/datocms";
-
-const HOMEPAGE_QUERY = `query HomePage {
-                                allProjects {
-                                  id
-                                  title
-                                  slug
-                                  businessType
-                                  tags
-                                  projectStatus
-                                  featured
-                                  description
-                                  skyview {
-                                    basename
-                                    filename
-                                    url
-                                  }
-                                  creativeProcess {
-                                    basename
-                                    filename
-                                    url
-                                  }
-                                  logo {
-                                    basename
-                                    filename
-                                    url
-                                  }
-                                  logoBackground {
-                                    hex
-                                  }
-                                  colorPalette
-                                  fonts
-                                  techsFront
-                                  techsBack
-                                  devices
-                                }
-                        }`;
-
 
 export default function Index({projects}) {
   return (
@@ -60,21 +23,49 @@ export default function Index({projects}) {
 
 export async function getServerSideProps(){
 
-  // const { API_URL } = process.env
-   
-  // const projects = await fetch(`${API_URL}/projects`).then(res => res.json())
-  // // console.log(projects)
-  // return{
-  //   props:{
-  //     projects: projects
-  //   }
-  // }
+  const HOMEPAGE_QUERY = `query HomePage {
+      allProjects {
+        id
+        title
+        slug
+        businessType
+        tags
+        projectStatus
+        featured
+        description
+        skyview {
+          basename
+          filename
+          url
+        }
+        creativeProcess {
+          basename
+          filename
+          url
+        }
+        logo {
+          basename
+          filename
+          url
+        }
+        logoBackground {
+          hex
+        }
+        colorPalette
+        fonts
+        techsFront
+        techsBack
+        devices
+      }
+  }`;
+
   const projects = await request({
     query: HOMEPAGE_QUERY,
     variables: { limit: 10 }
   });
-  // console.log(projects)
+  
   return {
     props: { projects }
   };
+  
 }
